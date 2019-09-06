f33ac00413e45edd101fd09383f6d7e780b2fe34
arxrezky <54320029+arxrezky@users.noreply.github.com>
2019-09-06 11:27:56 +0700

    Add files via upload

---
+++ form_pemesanan.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>DIGICOM</title>
+  </head>
+  <body>
+
+    <!-- Menu Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+      <div class="container">
+        <!-- <h3><i class="fas fa-cart-plus text-success mr-2"></i></h3> -->
+        <!-- <h3><img src="img/logo.png" width="50px"></h3> -->
+        <a class="navbar-brand font-weight-bold" href="#">DIGICOM</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mr-4">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Contact Us<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Form Pemesanan<span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button  class="btn btn-outline-dark my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+          </form>
+          <div class="icon mt-2">
+            <h5>
+              <i class="fas fa-cart-plus ml-3 mr-3" data-toggle="tooltip" title="Keranjang Belanja"></i>
+              <i class="fas fa-envelope mr-3" data-toggle="tooltip" title="Kotak Masuk"></i>
+              <i class="fas fa-bell mr-3" data-toggle="tooltip" title="Notifikasi"></i>
+            </h5>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Akhir Navbar -->
